80fc1d16e6a049dc4c390280f0e1b722efed0863
Zhong Yi Yan <83918638+LucaYan0506@users.noreply.github.com>
2022-10-18 12:57:44 +0000

    Write 2 functions, 1 that generate random months, 1 that generate random colors

---
+++ main.cs
@@ -0,0 +1,59 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    /*
+    string[] months = {"Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Now","Dec"};
+
+    for (int i = 0; i < 100; i++){
+      Random rnd = new Random();
+    
+      int month = rnd.Next(0,12);
+  
+      Console.WriteLine("Month is {0}",months[month]);      
+    }
+*/
+
+        string[] colors = {
+          "Azure",
+          "Aqua",
+          "Brown",
+          "Bronze",
+          "Blue",
+          "Black",
+          "Yellow",
+          "White",
+          "Wheat",
+          "Violet",
+          "Turquoise",
+          "Tomato",
+          "Teal",
+          "Silver",
+          "Salmon",
+          "Red",
+          "Purple",
+          "Pink",
+          "Orange",
+          "Olive",
+          "Navy blue",
+          "Navy",
+          "Maroon",
+          "Lime",
+          "Grey",
+          "Green",
+          "Gold",
+          "Dark blue",
+          "Cyan",
+          "Chocolate",
+          };
+
+    for (int i = 0; i < 100; i++){
+      Random rnd = new Random();
+    
+      int color = rnd.Next(0,colors.Length);
+  
+      Console.WriteLine("Color is {0}",colors[color]);      
+    }
+
+  }
+}
